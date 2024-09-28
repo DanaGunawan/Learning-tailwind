cb1a0c5a5624af1b45754aa6713d03421a1c4d6f
Dana Gunawan <dewakadekdana196@gmail.com>
2024-09-28 20:14:27 +0800

    Backgrounds and Display

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./fundamental/**/*.{html,js}" // This includes all HTML and JS files in the 'fundamental' folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
