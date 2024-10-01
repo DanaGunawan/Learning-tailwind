5261fa57d3e784b37f25b597b442f381b0ff7b49
Dana Gunawan <dewakadekdana196@gmail.com>
2024-10-01 18:17:24 +0800

    Project 1 Branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./fundamental/**/*.{html,js}" // This includes all HTML and JS files in the 'fundamental' folder
+    "./fundamental/**/*.{html,js}",
+    "./1. BrandingProject/**/*.{html,js}",
+    "./2 AgencyProject/**/*.{html,js}",
   ],
   theme: {
     extend: {},
