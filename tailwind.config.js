943d195a5152462fe8d7e63aefe8936eca7ee7d3
Dana Gunawan <dewakadekdana196@gmail.com>
2024-10-02 23:48:23 +0800

    project 2 fish Random

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     "./fundamental/**/*.{html,js}",
     "./1. BrandingProject/**/*.{html,js}",
-    "./2 AgencyProject/**/*.{html,js}",
+    "./2. AgencyProject/**/*.{html,js}",
   ],
   theme: {
     extend: {},
