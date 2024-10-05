43e259e8039d2275d6c487e72570b10e2f2198fe
Dana Gunawan <dewakadekdana196@gmail.com>
2024-10-05 23:50:47 +0800

    project 3 photography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./fundamental/**/*.{html,js}",
     "./1. BrandingProject/**/*.{html,js}",
     "./2. AgencyProject/**/*.{html,js}",
+    "./3. photographyProject/**/*.{html,js}",
   ],
   theme: {
     extend: {},
